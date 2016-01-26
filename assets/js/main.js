961765de3288ba708973c07bdf0f8ee8568eabd0
Ihor Orlovskyi <ihororlovskyi@gmail.com>
2016-01-26 03:03:50 +0200

    add jquery.lazyload and update html

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+/**
+* lazyload.js
+* https://github.com/tuupola/jquery_lazyload
+*/
+$(function() {
+    $("img.lazy").lazyload({
+      // threshold : 0,
+      effect : "fadeIn",
+      // placeholder : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
+    });
+});

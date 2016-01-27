2e9a262187fdf7bb2f044904225a1b8343a89aea
e.koval <e.koval@lucky-labs.com>
2016-01-27 18:33:21 +0200

    upd files

--- gulpfile.js
+++ gulpfile.js
@@ -13,7 +13,7 @@ var messages = {
  */
 gulp.task('jekyll-build', function (done) {
     browserSync.notify(messages.jekyllBuild);
-    return cp.spawn('jekyll', ['build'], {stdio: 'inherit'})
+    return cp.spawn('jekyll.bat', ['build'], {stdio: 'inherit'})
         .on('close', done);
 });
 
@@ -56,7 +56,7 @@ gulp.task('sass', function () {
  */
 gulp.task('watch', function () {
     gulp.watch('_scss/**/*', ['sass']);
-    gulp.watch(['*.html', '_layouts/*.html', '_includes/*.html', '_posts/**/*', 'assets/img/**/*', 'assets/js/**/*'], ['jekyll-rebuild']);
+    gulp.watch(['*.html', '_layouts/*.html', '_includes/**/*.html', '_posts/**/*', 'assets/img/**/*', 'assets/js/**/*'], ['jekyll-rebuild']);
 });
 
 /**

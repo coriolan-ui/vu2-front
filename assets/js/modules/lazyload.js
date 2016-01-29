17d34b441a3e2b1b396eb6f162b368773213ed74
Igor <igor@Mac-mini-User.local>
2016-01-29 13:41:17 +0200

    User Panel..

--- assets/js/modules/lazyload.js
+++ assets/js/modules/lazyload.js
@@ -6,6 +6,6 @@ $(function() {
     $("img.js-lazy").lazyload({
         // threshold : 0,
         effect : "fadeIn",
-        // placeholder : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
+         placeholder : "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsQAAA7EAZUrDhsAAAANSURBVBhXYzh8+PB/AAffA0nNPuCLAAAAAElFTkSuQmCC"
     });
 });
